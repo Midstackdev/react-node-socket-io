f78848ec5aedf415d51003cf0cc455bec443cf00
Midstackdev <midstackdev@gmail.com>
2021-10-30 00:13:19 +0000

    design

---
+++ client/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import './App.css'
+import Card from './components/card/Card';
+import Navbar from './components/navbar/Navbar';
+import { posts } from './data'
+
+const App = () => {
+  const [username, setUsername] = useState('')
+  const [user, setUser] = useState('')
+
+  console.log(user)
+
+  return (
+    <div className="container">
+      {user ? (
+        <>
+          <Navbar />
+          {posts.map(post => (
+            <Card key={post._id} post={post}/>
+          ))}
+          <span className="username">{user}</span>
+        </>
+      ) : (
+
+        <div className="login">
+          <input type="text" placeholder="username" onChange={e => setUsername(e.target.value)} />
+          <button onClick={() => setUser(username)}>Login</button>
+        </div> 
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/data.js
@@ -0,0 +1,18 @@
+export const posts = [
+    {
+        _id: '1',
+        username: 'smithy',
+        fullname: 'Alfred Smith',
+        userImg: 'https://images.pexels.com/photos/3748221/pexels-photo-3748221.jpeg?cs=srgb&dl=pexels-yogendra-singh-3748221.jpg&fm=jpg',
+        postImg: 'https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?cs=srgb&dl=pexels-stein-egil-liland-3408744.jpg&fm=jpg',
+        description: 'Nice pants for the office'
+    },
+    {
+        _id: '2',
+        username: 'dosty',
+        fullname: 'Fringe Doser',
+        userImg: 'https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg?cs=srgb&dl=pexels-mateus-souza-3586798.jpg&fm=jpg',
+        postImg: 'https://images.pexels.com/photos/3876407/pexels-photo-3876407.jpeg?cs=srgb&dl=pexels-tomas-anunziata-3876407.jpg&fm=jpg',
+        description: 'Nice shirts for the office'
+    },
+]
